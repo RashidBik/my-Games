27b7d720204877d85f8dc96aef3bb215bed8c7d6
rashidbik <abdurashid.bigzad1999@gmail.com>
2022-10-25 21:04:38 +0430

    distance fixed

--- src/minesweeper/mineApp.svelte
+++ src/minesweeper/mineApp.svelte
@@ -25,6 +25,28 @@
         grid[x][y].bomb = true;
                 
     }
+
+    function countDistance(i, j) {
+        if (grid[i][j].bomb) {
+            grid[i][j].distance = -1
+            return;
+
+        }
+        let count = 0 ;
+        for (let x = 0; x < 1; x++) {
+            for (let y = 0; y <= 1; y++) {
+                try {
+                    if (grid[x+i][j+j].bomb) {
+                        count++;
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+            
+        }
+        grid[i][j].distance = count;
+    }
 </script>
 
 <style>
@@ -50,6 +72,15 @@
     }
     .game > div.bomb:before{
         content: "";
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 20px;
+        height: 20px;
+        background: #111;
+        /* border: 1px solid black; */
+        border-radius: 50%;
     }
 </style>
 
