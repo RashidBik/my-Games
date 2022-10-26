eb61cfe2a372e28c155f8eae808733e3e1a284e9
rashidbik <abdurashid.bigzad1999@gmail.com>
2022-10-26 09:42:43 +0430

    request was mounted

--- src/jokes/JokeApp.svelte
+++ src/jokes/JokeApp.svelte
@@ -1,12 +1,24 @@
 <script>
+    import {getRandomJoke} from './request';
     import Form from "./Form.svelte";
     import Jokes from "./Jokes.svelte";
-
-
+    import { onMount } from 'svelte';
+let randomJoke;
+let mode = 'random';
+onMount(async()=> {
+    randomJoke  =  await onRandomJoke();
+});
+async function onRandomJoke() {
+    try {
+        randomJoke = await getRandomJoke();
+    } catch (error) {
+        alert(error + "There is an error");
+    }
+}
 </script>
 
 <div>
     <Form />
-    <button>search</button>
+    <button class="bg-gray-300">randomJoke</button>
     <Jokes />
 </div>
